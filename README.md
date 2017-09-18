a9fb2a32531c84507744ca40430a0f7260b3fc2a
Roberth Loaiza <pauloaiza@hotmail.es>
2017-09-18 17:32:12 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# roberth.loaiza.SVC.miw.upm.es
+#### Asignatura: *Ingeniería Web: Visión General*
+#### [Máster en Ingeniería Web por la U.P.M.](http://miw.etsisi.upm.es)
+
+### Tecnologías necesarias
+* Java
+* Maven
+* Eclipse
+* GitHub
+* Travis-CI
+* Sonarcloud
---
+++ scripts/test.bat
@@ -0,0 +1,21 @@
+@echo off
+set workspace=D:\MASTER\Ingenieria web vision general\workspace\eclipse\roberth.loaiza.svc.miw.upm.es
+::Las variables de entorno se pueden configurar directamente en Windows cuando se tienen los permisos necesarios
+set JAVA_HOME=D:\Program Files\Java\jdk1.8.0_131
+set M2_HOME=D:\Program Files\apache-maven-3.5.0\bin
+set PATH=%PATH%;%JAVA_HOME%\bin;%M2_HOME%\bin
+echo -----------------------------------------
+echo . (C) MIW - UPM
+echo -----------------------------------------
+echo .
+echo Workspace --- %workspace%
+echo JAVA_HOME --- %JAVA_HOME%
+echo M2_HOME   --- %M2_HOME%
+echo .
+d:
+cd %workspace%
+echo ============ mvn clean test (profile: develop) =======================================================
+echo .
+call mvn clean test --settings settings.xml
+
+pause
