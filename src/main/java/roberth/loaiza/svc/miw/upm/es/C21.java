5086bf70e37e831bd96210355c9db9c97ed700c1
Roberth Loaiza <pauloaiza@hotmail.es>
2017-09-18 17:41:24 +0200

    Create class C21

---
+++ src/main/java/roberth/loaiza/svc/miw/upm/es/C21.java
@@ -0,0 +1,14 @@
+package roberth.loaiza.svc.miw.upm.es;
+
+public class C21 {
+
+    public static String m1() {
+        
+        return "m1";
+    }
+
+    public static String m2() {
+        
+        return "m2";
+    }
+}
