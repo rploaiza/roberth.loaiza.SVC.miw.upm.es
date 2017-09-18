99f4f0b27e83ef7136780228019702aa519eb174
Roberth Loaiza <pauloaiza@hotmail.es>
2017-09-18 17:44:37 +0200

    Create class C31

---
+++ src/main/java/roberth/loaiza/svc/miw/upm/es/C31.java
@@ -0,0 +1,15 @@
+package roberth.loaiza.svc.miw.upm.es;
+
+public class C31 {
+    
+    public static String m1() {
+        
+        return "m1";
+    }
+
+    public static String m2() {
+        
+        return "m2";
+    }
+
+}
