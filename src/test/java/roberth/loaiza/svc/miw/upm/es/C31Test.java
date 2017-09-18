3076ebe26198f7454e6eafb4d5cc6681d4caf0d7
Roberth Loaiza <pauloaiza@hotmail.es>
2017-09-18 17:46:31 +0200

    Create class C31Test

---
+++ src/test/java/roberth/loaiza/svc/miw/upm/es/C31Test.java
@@ -0,0 +1,28 @@
+package roberth.loaiza.svc.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import roberth.loaiza.svc.miw.upm.es.C31;
+
+public class C31Test {
+    
+       
+    @Before
+    public void before() {
+         new C31();
+    }
+    
+    @Test
+    public void testM1() {
+        assertEquals("m1", C31.m1());
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals("m2", C31.m2());
+    }
+
+}
